1cf26d82fd964a8a47e5c01352fd993dee5350a5
nandhini <nandhini.g21@wipro.com>
2017-06-09 11:56:30 +0000

    first commit

--- server/index.js
+++ server/index.js
@@ -1,33 +1,28 @@
 let express = require('express'),
     path = require('path');
-    favicon = require('serve-favicon'),
+favicon = require('serve-favicon'),
     logger = require('morgan'),
     bodyParser = require('body-parser'),
     routes = require('./routes/index');
-
 const app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -39,7 +34,6 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
@@ -49,6 +43,4 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
-
 module.exports = app;
---
+++ server/public/js/add_data.js
@@ -0,0 +1,5 @@
+console.log("hi");
+function fnadd(e)
+{
+    console.log("222");
+}
