babd2edf9b29dd8bf75cb21ec152e96c97cab5f4
nandhini <nandhini.g21@wipro.com>
2017-06-12 06:58:59 +0000

    first commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+ var q;
 /* POST home page. */
 router.post('/', function(req, res, next) {
     var str = req.body.searchtext;
@@ -8,10 +9,12 @@ router.post('/', function(req, res, next) {
 });
 
 router.get('/search',function(req, res) {
-    console.log(req.query.moviename);
+   
     request.get('https://api.themoviedb.org/3/search/movie?api_key=360fb236234213ff65f11eac623fb645&language=en-US&query='+req.query.moviename+ '&include_adult=false', function(err, response, body) {
         if (!err && response.statusCode == 200) {
-            res.json(response.body);
+         
+          q = res.json(response.body);
+           
         }
         else {
             res.send('error occured');
